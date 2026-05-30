2b4ca4b52c086919a42124250917a685bbdd6c1e
lowyh04 <ylow0041@student.monash.edu>
2026-05-30 18:14:08 +0800

    Create new visualisations.js file - Separate the code for embedding chart from the html file

---
+++ js/visualisations.js
@@ -0,0 +1,44 @@
+var line_chart = "js/yearly_import_and_export_line_chart.vg.json";
+var waterfall_chart = "js/yearly_trade_balance_difference_waterfall_chart.vg.json"
+var bump_chart = "js/import_partner_bump_chart.vg.json";
+var stacked_bar = "js/value_of_import_stacked_bar.vg.json";
+var bubble_chart = "js/trade_balance_usd_exchange_rate_trade_to_gdp_ratio_bubble_chart.vg.json";
+var choropleth_map = "js/countries_imports_of_goods_GDP_choropleth_map.vg.json";
+var multi_line_chart = "js/import_electrical_electronic_products_multi_line.vg.json";
+var donut_chart = "js/malaysia_2024_export_value_donut_chart.vg.json";
+var stream_graph = "js/malaysia_2024_epxort_products_streamgraph.vg.json";
+var flow_map = "js/malaysia_2024_electrical_machinery_export_countries_flow_map.vg.json";
+var lollipop_chart = "js/malaysia_2025_export_product_growth_rate_lollipop_chart.vg.json";
+
+vegaEmbed('#line_chart', line_chart, {actions: false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#waterfall_chart', waterfall_chart, {actions: false}).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#bump_chart', bump_chart, {actions: false}).then(function(result){
+}).catch(console.error);
+
+vegaEmbed('#stacked_bar', stacked_bar, {actions: false}).then(function(result){
+}).catch(console.error);
+
+vegaEmbed('#bubble_chart', bubble_chart, {actions: false}).then(function(result){
+}).catch(console.error)
+
+vegaEmbed('#choropleth_map', choropleth_map, {actions: false}).then(function(result){
+}).catch(console.error)
+
+vegaEmbed('#multi_line_chart', multi_line_chart, {actions: false}).then(function(result){
+}).catch(console.error)
+
+vegaEmbed('#donut_chart', donut_chart, {actions: false}).then(function(result){
+}).catch(console.error)
+
+vegaEmbed('#stream_graph', stream_graph, {actions: false}).then(function(result){
+}).catch(console.error)
+
+vegaEmbed('#flow_map', flow_map, {actions: false}).then(function(result){
+}).catch(console.error)
+
+vegaEmbed('#lollipop_chart', lollipop_chart, {actions: false}).then(function(result){
+}).catch(console.error)
